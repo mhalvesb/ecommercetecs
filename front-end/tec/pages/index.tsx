91aa0668516852a7f0e9d2a68c00fdc70dffe494
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-06-30 16:39:58 -0300

    adicionadas mais seções a pagina

--- front-end/tec/pages/index.tsx
+++ front-end/tec/pages/index.tsx
@@ -1,11 +1,20 @@
 'use client'
 
 import "../global.modules.css";
-import { Content, FirstHeader, FirstUl, LineInfos, MainContent, SecondUl } from "./styles/index.styled";
-import Logo from "../assets/images/logotipo.png";
+import { Content, FirstHeader, FirstUl, LineInfos, MainContent, NavSec, ProductBox, Products, SecondContent, SecondUl, Stars, SupportSec } from "./styles/index.styled";
+import NotebookImg from "../assets/images/Notebook_PNG_Clip_Art-3074.png";
+import Acessories from "../assets/images/Gaming-Headset-PNG-HD-Image-1.png"
+import Cameras from "../assets/images/photo_camera_PNG101601.png"
+import Estrelas from "../assets/images/ESTRELAS.png";
+import Samsung from "../assets/images/samsungnotebook.png";
+import Headset from "../assets/images/headsetpng.png";
+import Intelbras from "../assets/images/camerapng.png";
+import Tablet from "../assets/images/36-tablet-png-image.png";
+
 export default function Home(){
     return(
-        <Content>
+        <div>
+            <Content>
             <FirstHeader>
             
             <FirstUl>
@@ -31,7 +40,86 @@ export default function Home(){
             <MainContent>
 
             </MainContent>
-        </Content>
+            </Content>
+
+            <SecondContent>
+                <NavSec>
+                    <div>
+                        <img src={NotebookImg.src}></img>
+                        <span>
+                            <h2>Notebooks</h2>
+                            <p>SHOP NOW ➔</p>
+                        </span>
+                    </div>
+                    <div>
+                    <img src={Acessories.src}></img>
+                        <span>
+                            <h2>Acessories</h2>
+                            <p>SHOP NOW ➔</p>
+                        </span>
+                    </div>
+                    <div>
+                    <img src={Cameras.src}></img>
+                        <span>
+                            <h2>Cameras</h2>
+                            <p>SHOP NOW ➔</p>
+                        </span>
+                    </div>
+                </NavSec>
+                <h1>New Products</h1>
+                <Products>
+                    <ProductBox>
+                        <img src={Samsung.src}></img>
+                        <p>Notebook</p>
+                        <h2>Notebook Samsung I7-4730 4GB RAM SSD 128GB + HDD 500GB</h2>
+                        <span>R$ 999,98</span>
+                        <button>BUY</button>
+                        <Stars>
+                            <img src={Estrelas.src}></img>
+                        </Stars>
+                    </ProductBox>
+
+                    <ProductBox>
+                        <img src={Headset.src}></img>
+                        <p>Acessories</p>
+                        <h2>HeadSet gamer stereo</h2>
+                        <span>R$ 51,99</span>
+                        <button>BUY</button>
+                        <Stars>
+                            <img src={Estrelas.src}></img>
+                        </Stars>
+                    </ProductBox>
+                    <ProductBox>
+                        <img src={Intelbras.src}></img>
+                        <p>Camera</p>
+                        <h2>Camera intelbras full hd 60fps</h2>
+                        <span>R$ 135,00</span>
+                        <button>BUY</button>
+                        <Stars>
+                            <img src={Estrelas.src}></img>
+                        </Stars>
+                    </ProductBox>
+                    <ProductBox>
+                        <img src={Tablet.src}></img>
+                        <p>Tablet</p>
+                        <h2>TABLET 4GB RAM TELA 12" ARMAZENAMENTO 256GB</h2>
+                        <span>R$ 344,99</span>
+                        <button>BUY</button>
+                        <Stars>
+                            <img src={Estrelas.src}></img>
+                        </Stars>
+                    </ProductBox>
+                </Products>
+            </SecondContent>
+
+            <SupportSec>
+               <div>
+               <h1>SUPORTE TÉCNICO <br></br>24HORAS</h1>
+               <button>Entrar em contato</button>                      
+               </div>
+            </SupportSec>
+
+        </div>
         
         
 
--- front-end/tec/pages/styles/index.styled.ts
+++ front-end/tec/pages/styles/index.styled.ts
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import Banner from "../../assets/images/pixelcut-export.jpeg";
-
+import Support from "../../assets/images/360_F_330901393_aOO2hcxJLCsahxjLcenapfQFqbsZT5Uu.jpg";
 export const Content = styled.div`
     width: 100%;
     height: 100vh;
     background-image: url(${Banner.src});
     background-repeat: no-repeat;
     background-size: cover;
+    background-position: center;
     position: relative;
 `;
 
@@ -62,12 +63,186 @@ export const SecondUl = styled.ul`
 
 export const LineInfos = styled.div`
     width: 100%;
-    height: 80px;
+    height: 60px;
     background-color: #111;
     position: absolute;
     bottom: 0;
+
+    ul{
+        display: flex;
+        list-style-type: none;
+        justify-content: space-around;
+    }
+
+    li{
+        color: #fff;
+        font-size: 25px;
+        text-transform: uppercase;
+    }
 `;
 
 export const MainContent = styled.main`
 
+`;
+
+
+export const SecondContent = styled.section`
+    width: 100%;
+    height: auto;
+    padding-bottom: 200px;
+    background-color: #D9D9D9;
+
+    h1{
+        font-size: 32px;
+        padding-left: 20px;
+        margin: 40px 0px;
+        text-transform: uppercase;
+        font-weight: 900;
+    }
+`;
+
+export const NavSec = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 0px 20px;
+    div{
+        margin-top: 60px;
+        background-color: #999;
+        width: 500px;
+        height: 300px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        img{
+            height: 275px;
+        }
+
+        span{
+            position: absolute;
+            height: 100%;
+            width: 50%;
+            background-color: #960018;
+            left: 0;
+            opacity: 75%;
+            border-radius: 0px 100px 0px 0px;
+            color: #fff;
+            display: flex;
+            padding-top: 50px;
+            padding-left: 20px;
+            flex-direction: column;
+            cursor: pointer;    
+            transition: all 0.5s ease-out;
+            &:hover{
+                width: 90%;
+            }
+        }
+
+        
+        h2{
+            font-size: 40px;
+            margin: 0;
+            padding: 0;
+        }
+        p{
+            font-size: 20px;
+        }
+    }
+`;
+
+export const Products = styled.div`
+display: flex;
+padding: 0px 20px;
+align-items: center;
+justify-content: space-between;
+text-align: center;
+text-transform: uppercase;
+position: relative;
+    img{
+        height: 250px;
+    }
+
+    p{
+        color: #999999;
+        font-size: 16px;
+        font-weight: 400;
+        margin: 10px 0px;
+    }
+    h2{
+        font-weight: 900;
+        margin: 10px 0px;
+    }
+    span{
+        display: block;
+        color: #FF0000;
+        font-weight: 700;
+        margin: 10px 0px;
+    }
+    button{
+        width: 90px;
+        height: 30px;
+        background-color: #000;
+        border: none;
+        cursor: pointer;
+        font-weight: 700;
+        border-radius: 5px;
+        color: #fff;
+        margin: 10px 0px;
+    }
+`;
+
+export const ProductBox = styled.div`
+        width: 400px;
+        height: 500px;
+        background-color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+`;
+
+export const Stars = styled.div`
+margin-top: auto;
+    img{
+        
+        height: 16px;
+    }
+`;
+
+export const SupportSec = styled.section`
+    background-image: url(${Support.src});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 728px;
+    position: relative;
+    text-align: center;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    div{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: auto;
+    }
+    h1{
+        margin: 0px;
+        padding: 0px;
+        font-weight: 900;
+        font-size: 100px;
+    }
+    button{
+        width: 250px;
+        height: 60px;
+        border: none;
+        background-color: #000;
+        color: #fff;
+        font-weight: 900;
+        text-transform: uppercase;
+        font-size: 20px;
+        margin-top: 30px;
+        cursor: pointer;
+    }
 `;
