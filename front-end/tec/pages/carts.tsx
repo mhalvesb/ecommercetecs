7c8a79aee0ab62431f0145606b9964176873be47
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-07-04 17:12:07 -0300

    adicionado pagina de carrinhos

--- front-end/tec/pages/carts.tsx
+++ front-end/tec/pages/carts.tsx
@@ -1,24 +1,30 @@
+import { CartHeader, CartItems, Container, FreteDiv, Item, ItemImgAndName, Steps, StepsImage, TotalDiv, TotalItems, ProductDiv, Frete, FreteInput } from "./styles/cart.styled";
+import "../global.modules.css";
 
-
-
+import Sacola from "../assets/images/icons/sacola-de-comprasazul.png";
+import User from "../assets/images/icons/perfil-de-usuario.png";
+import Notebook from "../assets/images/Samsung-lança-notebook-Galaxy-Book-Go-no-Brasil-845x563.png";
 export default function Cart(){
     return(
-        <div>
-            <div>
+        <Container>
+            <CartHeader>
                 <h1>Logotipo</h1>
-            </div>
-            <div>
+            </CartHeader>
+            <Steps>
                 <ul>
-                    <li>Sacola</li>
-                    <li>Identificação</li>
-                    <li>Pagamento</li>
+                    <hr></hr>
+                    <li><StepsImage style={{borderColor: "#0047AB"}}><img src={Sacola.src}></img></StepsImage>Sacola</li>
+                    <li><StepsImage><img src={User.src}></img></StepsImage>Identificação</li>
+                    <li><StepsImage><img src={User.src}></img></StepsImage>Pagamento</li>
                 </ul>
-            </div>
-            <div>
+            </Steps>
+            <CartItems>
                 <h6>Sacola</h6>
-                <div>
-                    <img src=""></img>
-                    <h4>Cartucho HP 664 colorido</h4>
+                <Item>
+                    <ItemImgAndName>
+                        <img src={Notebook.src}></img>
+                        <h4>Notebook Samsung I5-4370 4GB RAM SSD 128GB + HDD 500GB WINDOWS 11</h4>
+                    </ItemImgAndName>
                     <div>
                         <select>
                             <option>1</option>
@@ -33,26 +39,46 @@ export default function Cart(){
                         <h4>R$ 72,99 no Pix</h4>
                         <h5>R$ 75,99 no Cartão</h5>
                     </div>
-                </div>
-            </div>
-            <div>
-                <div>
+                </Item>
+                <Item>
+                    <ItemImgAndName>
+                        <img src={Notebook.src}></img>
+                        <h4>Cartucho HP 664 colorido</h4>
+                    </ItemImgAndName>
+                    <div>
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                        <p>Excluir</p>
+                    </div>
+                    <div>
+                        <h4>R$ 72,99 no Pix</h4>
+                        <h5>R$ 75,99 no Cartão</h5>
+                    </div>
+                </Item>
+            </CartItems>
+            <TotalItems>
+                <Frete>
                     <h3>Frete para o CEP</h3>
-                    <input type="text" placeholder="00000-000"/>
-                </div>
+                    <FreteInput><input type="text" placeholder="00000-000"/> <button>OK</button></FreteInput>
+                </Frete>
                 <div>
-                    <div>
-                        <h3>Frete Total</h3> <h3>R$ 21,99</h3>
-                    </div>
-                    <div>
-                        <h3>Produtos</h3> <h3>R$ 528,99</h3>
-                    </div>
-                    <div>
-                        <h2>Total</h2> <h2>R$ 600,00</h2>
-                    </div>
+                    <FreteDiv>
+                        <h3>Frete Total: </h3> <span> R$ 21,99</span>
+                    </FreteDiv>
+                    <ProductDiv>
+                        <h3>Produtos: </h3> <span> R$ 528,99</span>
+                    </ProductDiv>
+                    <TotalDiv>
+                        <h2>Total: </h2> <span> R$ 600,00</span>
+                    </TotalDiv>
                     <button>Comprar</button>
                 </div>
-            </div>
-        </div>
+            </TotalItems>
+        </Container>
     )
 }
---
+++ front-end/tec/pages/styles/cart.styled.ts
@@ -0,0 +1,230 @@
+import styled from "styled-components";
+
+
+
+
+
+export const Container = styled.main`
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    
+`;
+
+export const CartHeader = styled.header`
+    background-color: #111;
+    width: 100vw;
+    color: #fff;
+    padding: 0 10vw;
+`;
+
+export const Steps = styled.div`
+    margin: 10px auto;
+    width: 100%;
+    padding: 0 25vw;
+    ul{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+        position: relative;
+        width: 100%;
+    }
+    hr{
+        position: absolute;
+        width: 95%;
+        margin-left: 2px;
+        height: 3px;
+        top: 16px;
+        z-index: 0;
+        background-color: #111;
+        border: none;
+        &::before{
+    content: "";
+    width: 20%;
+    position: absolute;
+    background-color: #0047AB;
+    z-index: 99;
+    height: 3px;
+    }
+    }
+    li{
+        margin: 0;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        z-index: 2;
+    }
+    
+`;
+
+export const StepsImage = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+border-radius: 9999px;
+border: 1px solid #111;
+width: 50px;
+height: 50px;
+background-color: #D9D9D9;
+z-index: 2;
+    img{
+        width: 32px;
+        z-index: 2;
+    }
+`;
+
+
+export const CartItems = styled.div`
+    width: 100vw;
+    margin: 10px 0px;
+    padding: 0 25vw;
+
+    h6{
+        font-size: 25px;
+        font-weight: 700;
+        color: #0047AB;
+        margin: 0;
+        padding: 0;
+        margin: 20px 0px;
+    }
+`;
+
+
+export const Item = styled.div`
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #999;
+    padding-bottom: 10px;
+    margin: 50px 0px;
+    p{
+        font-size: 14px;
+        margin: 0;
+        padding: 0;
+        text-align: center;
+        cursor: pointer;
+        margin: 5px 0px;
+    }
+    select{
+        width: 51px;
+        height: 30px;
+        border: 1px solid #999;
+        background: transparent;
+        cursor: pointer;
+        outline: none;
+        margin: 0px 20px;
+    }
+
+    h4, h5{
+        margin: 0;
+        padding: 0;
+        
+    }
+    h4{
+        color: #000;
+        font-size: 16px;
+    }
+    h5{
+        color: #000;
+        font-size: 18px;
+    }
+`;
+
+export const ItemImgAndName = styled.div`
+    display: flex;
+    flex: 1;
+    h4{
+        margin: 0;
+        padding: 0;
+        color: #111;
+        font-weight: 500;
+        font-size: 16px;
+        margin-left: 20px;
+    }
+    img{
+        width: 100px;
+        height: auto;
+        background-color: #fff;
+    }
+`;
+
+
+export const TotalItems = styled.div`
+    display: flex;
+    margin: 0 25vw;
+    justify-content: space-between;
+    button{
+        width: 100%;
+        height: 50px;
+        background-color: #50C878;
+        color: #fff;
+        font-weight: 900;
+        border: none;
+        cursor: pointer;
+    }
+    span{
+        margin: 0;
+        padding: 0;
+        margin: 15px 0px;
+        font-size: 18px;
+        font-weight: 900;
+        color: #111;
+    }
+    h3, h4, h2{
+        margin: 15px 0px;
+        padding: 0;
+    }
+    padding-bottom: 20px;
+    border-bottom: 1px solid #000;
+`;
+
+export const FreteDiv = styled.div`
+    display: flex;
+    h3{
+        color: #555;
+        margin-right: 10px;
+        font-size: 16px;
+
+    }
+`;
+export const ProductDiv = styled.div`
+    display: flex;
+    h3{
+        color: #555;
+        margin-right: 10px;
+        font-size: 16px;
+    }
+`;
+export const TotalDiv = styled.div`
+    display: flex;
+    h2{
+        color: #111;
+        padding: 0;
+        margin-right: 10px;
+        font-size: 18px;
+        
+    }
+`;
+
+export const Frete = styled.div`
+    h3{
+        margin: 5px 0px;
+        color: #555;
+    }
+    input{
+        width: 150px;
+        height: 30px;
+        border: 1px solid #999;
+    }
+`;
+
+export const FreteInput = styled.div`
+    display: flex;
+    button{
+        height: auto;
+    }
+`;
